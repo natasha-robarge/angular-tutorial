24ef616f1977ebc6cbb87131e4dd8afe01d9cc01
Natasha Robarge <natasha.robarge@headspring.com>
2018-07-20 10:03:14 -0500

    Created addItem to add items to shopping list- imported FormModules from Angular/forms so that each time I click the submit button, the form doesn't automatically refresh because submit buttons automatically refresh the DOM, hence why in normal React, you use event.preventDefault before doing anything else in your functions.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,7 +15,8 @@ import { Recipe } from '../../shared/recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70')
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70'),
+    new Recipe('Another Test Recipe', 'This is simply a test.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70')
   ];
 
   constructor() { }
