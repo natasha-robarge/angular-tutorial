1281803d8252a2e41f204ac0b833f9c2bebd2d04
Natasha Robarge <natasha.robarge@headspring.com>
2018-07-20 12:30:27 -0500

    Added directive file to open and close bootstrap dropdown

---
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,28 @@
+import {
+  Directive,
+  HostListener,
+  HostBinding,
+  Renderer2,
+  ElementRef
+} from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+
+  constructor (private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostBinding('class.show')isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+
+    let part = this.el.nativeElement.querySelector('.dropdown-menu');
+    if (this.isOpen)
+      this.renderer.addClass(part, 'show');
+    else
+      this.renderer.removeClass(part, 'show');
+  }
+}
