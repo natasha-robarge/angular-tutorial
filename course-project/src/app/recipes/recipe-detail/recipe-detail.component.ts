f7c728a25523ae7e8ffc889cf21558a241f42e35
Natasha Robarge <natasha.robarge@headspring.com>
2018-07-24 15:28:20 -0500

    Added new getRecipe method that takes in an id from the recipes array and selects the recipe in the url. Added a subscribe event to subscribe to the route and see if the id in the route path matches the id[index] in the recipes array

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,8 @@
 import {
   Component,
   OnInit,
-  Input
 } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Recipe } from '../../shared/recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -13,11 +13,20 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
