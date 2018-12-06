f78f03ee89a5def7d3058d3865f2e23d36e6dda7
Natasha Robarge <natasha.robarge@headspring.com>
2018-12-06 11:52:40 -0600

    create routing module for recipes and add it to recipes module

--- course-project/src/app/recipes/recipes.module.ts
+++ course-project/src/app/recipes/recipes.module.ts
@@ -6,6 +6,8 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,10 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
     RecipeEditComponent,
     RecipeDetailComponent,
     RecipeItemComponent
+  ],
+  imports: [
+    SharedModule,
+    RecipesRoutingModule,
   ]
 })
-export class RecipesModule {
-  constructor() { }
-}
+export class RecipesModule { }
