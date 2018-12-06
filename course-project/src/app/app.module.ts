8954238b2ccb66651a75364d22949a63d35d6275
Natasha Robarge <natasha.robarge@headspring.com>
2018-12-06 14:31:10 -0600

    move header and home folders into core folder

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -14,11 +14,13 @@ import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
